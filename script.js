f7c730c6a10f27f51f6d12ff1439859f78ac6b19
Mayhoral8 <85770878+Mayhoral8@users.noreply.github.com>
2022-05-10 19:27:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 'use-strict'
 const toDo = document.querySelector('.to-do');
-const create = document.querySelector('.create');
+const create = document.querySelector('.createBtn');
 let body = document.querySelector('.body');
 const inputs = document.querySelector('.inputs');
 const clearAll = document.querySelector('.clear');
+
+ 
 
 
 create.addEventListener('click', function(){
@@ -11,7 +13,6 @@ create.addEventListener('click', function(){
     if (toDo.value === ''){
         toDo.textContent = 'Please Enter Text';
     } else {
-        // inputs.classList.add('border');
     let newToDo = document.createElement('button');
     let deletebtn = document.createElement('button');
     let editbtn = document.createElement('button');
@@ -21,8 +22,8 @@ create.addEventListener('click', function(){
       let createBtn;
     editbtn.textContent = 'Edit';
     doneBtn.style.marginLeft = '5px'
-    deletebtn.textContent = 'X';
-    deletebtn.style.width = '2%';
+    deletebtn.textContent = '❌';
+    deletebtn.style.width = '3%';
     newToDo.textContent = toDo.value;
     console.log(newToDo.style.placeContent);
     console.log(toDo.value);
@@ -38,44 +39,19 @@ create.addEventListener('click', function(){
     deletebtn.addEventListener('click', function (){
         inputs.removeChild(newToDo);
         inputs.removeChild(deletebtn);
-        // inputs.removeChild(editbtn);
         inputs.removeChild(doneBtn);
     })
 
-    // editbtn.addEventListener('click', function(){
-    //     let editInp = document.createElement('input');
-    //     createBtn = document.createElement('button');
-    //     deletebtn.classList.add('hidden');
-    //     doneBtn.classList.add('hidden');
-    //     inputs.appendChild(newToDo);  
-    //     inputs.replaceChild(editInp, newToDo);
-    //     createBtn = editbtn;
-    //     editInp.style.width = '62%'
-    //     createBtn.textContent = 'Create';
-        
-        
-    //         if ( deletebtn.classList.contains('hidden') && doneBtn.classList.contains('hidden')){
-    //             newToDo.classList.remove('hidden');
-    //             deletebtn.classList.remove('hidden');
-    //         doneBtn.classList.remove('hidden');
-    //         inputs.replaceChild(newToDo, editInp);
-    //         } else{
-    //             inputs.appendChild(newToDo);  
-    //             inputs.appendChild(editInp);
-    //             inputs.replaceChild(newToDo, editInp);
-    //             editInp.style.width = '62%';
-    //             deletebtn.classList.add('hidden');
-    //             doneBtn.classList.add('hidden');
-    //         }  
-    // })
     doneBtn.addEventListener('click', function(){
         // inputs.removeChild(editbtn);
         inputs.removeChild(deletebtn);
-        doneBtn.textContent = 'Completed';
+        doneBtn.textContent = 'Completed ✅';
+        doneBtn.classList.add('spacer');
         doneBtn.style.fontWeight = 'bolder';
         doneBtn.style.marginTop = '10px';
-        newToDo.style.backgroundColor ='green';
+        newToDo.style.backgroundColor ='rgb(40, 243, 47)';
     })
+
     clearAll.addEventListener('click', function(){
         inputs.removeChild(newToDo);
         inputs.removeChild(doneBtn);
@@ -84,4 +60,3 @@ create.addEventListener('click', function(){
     
 }
 });
-
